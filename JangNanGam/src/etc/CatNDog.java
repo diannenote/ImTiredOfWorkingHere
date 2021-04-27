5bf7fcabb69a986a93bff1554eaf257043eca9ef
KANG MIRI <564486_v@naver.com>
2021-04-27 15:58:48 +0900

    catNdog

---
+++ JangNanGam/src/etc/CatNDog.java
@@ -0,0 +1,24 @@
+package etc;
+
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+*/ 
+public class CatNDog {
+	public static void main(String[] args) {
+		
+	/*	System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|"); 
+		
+		System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+		*/
+		System.out.println("\\    /\\		|\\_/|\n )  ( ')	|q p|   /}\n(  /  )		( 0 )\"\"\"\\\n \\(__)|		|\"^\"`   |\n		||_/=\\__|");
+	    
+		
+	}
+}
+
