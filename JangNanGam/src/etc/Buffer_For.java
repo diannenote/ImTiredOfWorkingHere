418d2e2a6d27e9f80cc8ea2f659bf2b6589671d9
KANG MIRI <564486_v@naver.com>
2021-05-21 17:15:00 +0900

    NXA

--- JangNanGam/src/etc/Buffer_For.java
+++ JangNanGam/src/etc/Buffer_For.java
@@ -7,8 +7,8 @@ public class Buffer_For {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		//빠른 A+B
-/*
+//빠른 A+B
+		/*
 		StringTokenizer sk;
 		int N = Integer.parseInt(br.readLine());
 		for(int i = 0; i<N; i++) {
@@ -20,25 +20,25 @@ public class Buffer_For {
 		br.close();
 		bw.flush();
 		bw.close();
- */
-		// 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
-/*	
+		 */
+// 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+		/*	
 		int n = Integer.parseInt(br.readLine());
 		for(int i=1; i<=n; i++) {
 			bw.write(i+"\n");
 		}
 		bw.close();
-*/		
-		// 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
-/*		
+		 */		
+// 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+		/*		
 		int n = Integer.parseInt(br.readLine());
 		for(int i=n; i>0; i--) {
 			bw.write(i+"\n");
 		}
 		bw.close();
-*/
+ 		*/
 //		각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
-/*		
+		/*		
 		StringTokenizer sk;
 		int T = Integer.parseInt(br.readLine());
 		for(int i = 1; i<=T; i++) {
@@ -48,9 +48,9 @@ public class Buffer_For {
 			
 		}
 		bw.close();
-*/
+ 		*/
 //		"Case #x: A + B = C" 형식으로 출력
-/*
+		/*
 		int T = Integer.parseInt(br.readLine());
 		StringTokenizer sk;
         for(int i=0;i<T;i++){
@@ -61,9 +61,9 @@ public class Buffer_For {
             bw.write(String.format("Case #%d: %d + %d = %d\n",i+1,a,b,a+b));
         }
         bw.close();
-*/
+ 		*/
 //		첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
-/*		
+		/*		
 		int N = Integer.parseInt(br.readLine());
 		for(int i = 0; i<N; i++) {
 			for (int j=0; j<=i; j++) {
@@ -75,10 +75,10 @@ public class Buffer_For {
 		
 		bw.flush();
 		bw.close();
-*/
+		 */
 //	첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
 //	하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
-		int N = Integer.parseInt(br.readLine());
+	/*	int N = Integer.parseInt(br.readLine());
 		for(int i = 1; i<=N; i++) {
 			for(int j = 0; j < N-i; j++) {
 				bw.write(" ");
@@ -92,5 +92,23 @@ public class Buffer_For {
 		
 		bw.flush();
 		bw.close();
+	*/
+//		정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+//		첫째 줄에 N과 X가 주어진다.
+//		둘째 줄에 수열 A를 이루는 정수 N개가 주어진다. 
+		StringTokenizer sk= new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(sk.nextToken());
+        int X = Integer.parseInt(sk.nextToken());
+        int A[] = new int[N];
+        StringTokenizer arr = new StringTokenizer(br.readLine());
+        for(int i=0; i<N;i++) {
+        	A[i] = Integer.parseInt(arr.nextToken());
+        	if (A[i] < X) bw.write(A[i]+" ");
+        }
+        
+		br.close();	
+		bw.flush();
+		bw.close();
+
 	}
 }
