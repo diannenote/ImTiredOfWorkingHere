ace98dfd876eb0c3825c6e19ce7f61ae5225c88b
KANG MIRI <564486_v@naver.com>
2021-05-21 16:23:41 +0900

    star2

--- JangNanGam/src/etc/Buffer_For.java
+++ JangNanGam/src/etc/Buffer_For.java
@@ -63,10 +63,27 @@ public class Buffer_For {
         bw.close();
 */
 //		첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
-		
+/*		
 		int N = Integer.parseInt(br.readLine());
 		for(int i = 0; i<N; i++) {
 			for (int j=0; j<=i; j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		br.close();
+		
+		bw.flush();
+		bw.close();
+*/
+//	첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+//	하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 1; i<=N; i++) {
+			for(int j = 0; j < N-i; j++) {
+				bw.write(" ");
+			}
+			for(int k= 0; k<i; k++) {
 				bw.write("*");
 			}
 			bw.write("\n");
